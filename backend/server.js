d30cc3fdbbab92eada84d3cba0cf2d9e3513d7ff
Mohammed Salman Ali <mohammedsalmanali010@gmail.com>
2023-10-08 06:59:35 +0000

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,42 @@
-// backend/server.js
+// Backend server code (e.g., server.js)
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const app = express();
-const port = 3001; // Choose a port for your backend
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// Enable CORS for your frontend
-app.use(cors());
+const app = express();
+const port = process.env.PORT || 5000;
 
-// Define your authentication logic, routes, and middleware here
+app.use(cors());
+app.use(bodyParser.json());
+
+// Your user data and database connection logic
+const users = [
+  { id: 1, email: 'user@example.com', password: 'password123' },
+  // Add more user data as needed
+];
+
+// Secret key for JWT token generation
+const secretKey = 'your-secret-key';
+
+// Authentication endpoint
+app.post('/api/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // Find the user in your database by email
+  const user = users.find((u) => u.email === email);
+
+  if (!user || user.password !== password) {
+    // Unauthorized: Invalid credentials
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  // Generate a JWT token and send it in the response
+  const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+  res.json({ token });
+});
 
 app.listen(port, () => {
-  console.log(`Backend server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,46 +1,49 @@
-// src/components/Login.js
-
+// Import the useHistory hook
+import { useHistory } from 'react-router-dom';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
+  // Create state variables to store user input
   const [formData, setFormData] = useState({
-    username: '',
+    email: '',
     password: '',
   });
 
+  // Access the history object
+  const history = useHistory();
+
+  // Handle changes in the form input fields
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  // Handle form submission
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Add your login logic here
+
+    try {
+      // Make a POST request to your backend's authentication endpoint
+      const response = await axios.post('/api/login', formData);
+
+      // Handle successful login, e.g., store JWT token in local storage
+      localStorage.setItem('jwtToken', response.data.token);
+
+      // Redirect to a protected route or perform other actions
+      history.push('/dashboard'); // Redirect to '/dashboard'
+    } catch (error) {
+      console.error('Login error:', error);
+      // Handle login error, e.g., display an error message to the user
+    }
   };
 
   return (
     <div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Login</button>
+        {/* Input fields and form elements */}
+        {/* ... */}
       </form>
     </div>
   );
